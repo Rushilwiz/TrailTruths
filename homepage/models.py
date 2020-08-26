c0adf2fa300c2a2cb596dc739f488341fb5d4260
rushilwiz <rushilwiz@gmail.com>
2020-08-26 02:42:31 -0400

    working poll, models, and options

--- homepage/models.py
+++ homepage/models.py
@@ -3,11 +3,45 @@ from django.db import models
 # Create your models here.
 
 class Poll (models.Model):
-	askHi = models.BooleanField(default=True)
-	askLo = models.BooleanField(default=True)
-	askEmotion = models.BooleanField(default=True)
-	askEmotion = models.BooleanField(default=True)
+	ask_hi = models.BooleanField(default=True)
+	hi_text = models.CharField(max_length=50, blank=True, null=True)
+	
+	ask_lo = models.BooleanField(default=True)
+	lo_text = models.CharField(max_length=50, blank=True, null=True)
+	
+	ask_emotion = models.BooleanField(default=True)
+	emotion_text = models.CharField(max_length=50, blank=True, null=True)
+	
+	ask_name = models.BooleanField(default=True)
+	name_text = models.CharField(max_length=50, blank=True, null=True)
+	
+	ask_place = models.BooleanField(default=True)
+	place_text = models.CharField(max_length=50, blank=True, null=True)
+	
+	ask_question = models.BooleanField(default=False)
+	question_text = models.CharField(max_length=50, blank=True, null=True)
+
 	pub_date = models.DateTimeField('date published')
-	extraQuestion1 = models.CharField(max_length=50, blank=True, null=True)
-	extraQuestion2 = models.CharField(max_length=50, blank=True, null=True)
-	extraQuestion3 = models.CharField(max_length=50, blank=True, null=True)
+
+	def __str__(self):
+		return 'Current Poll'
+
+
+
+EMOTION_CHOICES= [
+    ('happy', 'Happy'),
+    ('meh', 'Meh'),
+    ('sad', 'Sad'),
+]
+
+class Answer (models.Model):
+	poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+	hi = models.CharField(max_length=200, blank=True, null=True)
+	lo = models.CharField(max_length=200, blank=True, null=True)
+	emotion = models.CharField(max_length=8, default='meh', choices=EMOTION_CHOICES, blank=True, null=True)
+	name = models.CharField(max_length=100, blank=True, null=True)
+	place = models.CharField(max_length=100, blank=True, null=True)
+	question = models.CharField(max_length=200, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+
+from .forms import PollForm
+from .models import Poll
 
 # Create your views here.
 
 def homepage (request):
-    return render(request, 'homepage/index.html')
+	poll = get_object_or_404(Poll)
+
+	if poll.ask_hi:
+		hi_text = poll.hi_text
+	else:
+		hi_text = None
+
+	if poll.ask_lo:
+		lo_text = poll.lo_text
+	else:
+		lo_text = None
+
+	if poll.ask_emotion:
+		emotion_text = poll.emotion_text
+	else:
+		emotion_text = None
+
+	if poll.ask_name:
+		name_text = poll.name_text
+	else:
+		name_text = None
+
+	if poll.ask_place:
+		place_text = poll.place_text
+	else:
+		place_text = None
+
+	if poll.ask_question:
+		question_text = poll.question_text
+	else:
+		question_text = None
+
+	if request.method == 'POST':
+		print(request.POST)
+		form = PollForm(request.POST)
+		if form.is_valid():
+			print("valid form!")
+			instance = form.save(commit=False)
+			instance.poll = poll
+			instance.save()
+			print(instance.pk)
+			return redirect('finish')
+		else:
+			messages.error(request, 'Looks like there were some problems with your form!', extra_tags='danger')
+			print("invalid form!")
+	else:
+		form = PollForm()
+
+	context = {
+		'form': form,
+		'hi_text': hi_text,
+		'lo_text': lo_text,
+		'emotion_text': emotion_text,
+		'place_text': place_text,
+		'name_text': name_text,
+		'question_text': question_text,
+	}
+
+	return render(request, 'homepage/index.html', context=context)
+
+def finish (request):
+	pass
