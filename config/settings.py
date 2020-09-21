cc018d1ee4a7e4276fd6845057e2f73bc4525677
Rushil Umaretiya <rushilwiz@gmail.com>
2020-09-21 19:17:56 -0400

    added new domain

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hilo.link']
+ALLOWED_HOSTS = ['hilo.link','hi-loproject.com']
 
 
 # Application definition
